985e4516a7338a36bba12519a5f00d0bd3842cde
Shmugeliuk Oleg <shm.oleh29@gmail.com>
2026-02-12 15:08:42 +0200

    Update CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: [process.env.CLIENT_URL!],
     credentials: true,
   });
   app.setGlobalPrefix('api');
